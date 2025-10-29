7372fc1269d012e14598bac378dbb11ad688a433
Denis Gonzalez <dev@degomon.com>
2025-10-29 15:11:31 -0600

    cobros-fecha-global

--- sql/col-consulting/fixing-utils/fix_cobros_duplicados.sql
+++ sql/col-consulting/fixing-utils/fix_cobros_duplicados.sql
@@ -1,21 +1,41 @@
 DO $$
-DECLARE 
-    -- declare a record type to hold the result of a cursor
-    payment record;
+DECLARE
+  -- declare a record type to hold the result of a cursor
+  payment record;
 BEGIN
-
-for payment in select pay.c_payment_id, pay.payamt
-from c_payment pay
-where pay.C_DocType_ID=1000050
-and not exists (
-	select local_id from legacy_cobro where local_id = pay.c_payment_id
-) loop
-    update c_payment set payamt = 0.00 where c_payment_id = payment.c_payment_id;
-    update C_AllocationLine set amount = 0.00 where c_payment_id = payment.c_payment_id;
-    raise notice 'Payment % has been set to 0.00', payment.c_payment_id;
-end loop;
-END $$
-
-
+  FOR payment IN
+  SELECT
+    pay.c_payment_id,
+    pay.payamt,
+    pay.datetrx
+  FROM
+    c_payment pay
+  WHERE
+    pay.C_DocType_ID = 1000050
+    AND pay.payamt != 0
+    AND NOT EXISTS (
+      SELECT
+        local_id
+      FROM
+        legacy_cobro
+      WHERE
+        local_id = pay.c_payment_id)
+      LOOP
+        UPDATE
+          c_payment
+        SET
+          payamt = 0.00
+        WHERE
+          c_payment_id = payment.c_payment_id;
+        UPDATE
+          C_AllocationLine
+        SET
+          amount = 0.00
+        WHERE
+          c_payment_id = payment.c_payment_id;
+        RAISE NOTICE 'Payment % - Date % has been set to 0.00', payment.c_payment_id, payment.datetrx;
+      END LOOP;
+END
+$$
 --- select * from C_AllocationLine
- -- where  C_AllocationLine_ID=1000628
+-- where  C_AllocationLine_ID=1000628
